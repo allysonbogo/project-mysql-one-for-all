6594a6dfb50ece7ef8983777ca395edd9a9450b8
Allyson Belli Bogo <ab.bogo@gmail.com>
2023-05-08 19:29:10 -0300

    refatora requisito 4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -2,7 +2,6 @@ SELECT
 	u.nome_pessoa_usuaria AS pessoa_usuaria,
     IF(h.data_reproducao > '2021-01-01', 'Ativa', 'Inativa') AS status_pessoa_usuaria
 FROM usuarios AS u
--- INNER JOIN historico AS h
 INNER JOIN (SELECT pessoa_usuaria_id, MAX(data_reproducao) data_reproducao FROM historico GROUP BY pessoa_usuaria_id) AS h
 ON u.pessoa_usuaria_id = h.pessoa_usuaria_id
 GROUP BY pessoa_usuaria, status_pessoa_usuaria
