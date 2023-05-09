0f978aa27650c49923704a06e3524dffaec58e43
Allyson Belli Bogo <ab.bogo@gmail.com>
2023-05-09 08:59:01 -0300

    requisito 10

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,25 @@
--- Descomente e altere as linhas abaixo:
+USE SpotifyClone;
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
+CREATE TABLE favoritadas 
+(
+    pessoa_usuaria_id	INT NOT NULL,
+    cancao_id	INT NOT NULL,
+    	CONSTRAINT PRIMARY KEY(pessoa_usuaria_id, cancao_id),
+      FOREIGN KEY (pessoa_usuaria_id) REFERENCES usuarios (pessoa_usuaria_id),
+      FOREIGN KEY (cancao_id) REFERENCES cancoes (cancao_id)
+) engine = InnoDB;
 
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO favoritadas (pessoa_usuaria_id, cancao_id) VALUES
+	('1', '3'),
+	('1', '6'),
+	('1', '10'),
+	('2', '4'),
+	('3', '1'),
+	('3', '3'),
+	('4', '7'),
+	('4', '4'),
+	('5', '10'),
+	('5', '2'),
+	('8', '4'),
+	('9', '7'),
+	('10', '3');
